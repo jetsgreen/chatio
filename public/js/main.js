d886a6267508c4eeee041b3a055c55806119fb5a
Felix Fernandez <fernandezfelix460.gmail.com>
2020-08-04 10:24:20 -0400

    fixed package.json file(homepage)

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@ const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
+
 // Get username and room from URL
 const { username, room} = Qs.parse(location.search, {
     ignoreQueryPrefix: true
