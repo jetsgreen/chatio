93244a6dff1f965f2b3e4f45c51a13ea061686d3
Felix Fernandez <fernandezfelix460.gmail.com>
2020-08-04 12:40:04 -0400

    update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./model/u
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = require('socket.io').listen(server);
 
 // set up access to public folder
 app.use(express.static(path.join(__dirname, 'public')));
