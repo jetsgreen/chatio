a3f062c16985fa2b2d8fe29f28c2201ab19013e7
Felix Fernandez <fernandezfelix460.gmail.com>
2020-08-06 11:04:27 -0400

    changed folder's name

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 
-const formatMessage = require('./model/messages');
-const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./model/users');
+const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
