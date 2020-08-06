601c7428fabab1821238ecf4de66b6beb1e5c5ac
Felix Fernandez <fernandezfelix460.gmail.com>
2020-08-06 13:21:14 -0400

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
-const socketio = require('socket.io');
+// const socketio = require('socket.io');
 
 const formatMessage = require('./utils/messages');
 const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
